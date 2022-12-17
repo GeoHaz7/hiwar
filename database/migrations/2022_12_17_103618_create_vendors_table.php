8d36b4fc10585fba21e70df14bf836244a3b03a5
George Hazboun <thenoob885@gmail.com>
2022-12-17 12:40:58 +0200

    first commit

---
+++ database/migrations/2022_12_17_103618_create_vendors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->increments('vendor_id');
+            $table->string('full_name');
+            $table->longText('bio');
+            $table->boolean('status');
+            $table->string('address');
+            $table->integer('phone');
+            $table->string('profile_image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendors');
+    }
+};
